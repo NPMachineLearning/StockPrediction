d2ddff8ae6bc7c6f462b2562520e461ab1e50d88
tomneo2004 <tomneo2004@gmail.com>
2024-01-26 12:35:06 +0800

    add basic fastapi, reorganized folders, update mongodb config setting, update docker-compose.yml

--- mongodb/init_stock_config.js
+++ mongodb/init_stock_config.js
@@ -1,3 +1,3 @@
 db = connect("mongodb://localhost/stocks");
-db.config.insertOne({ window: 1 });
+db.config.insertOne({ window: 7 });
 db.stocks.insertOne({ stock_symbols: ["GC=F"] });
---
+++ stock_service/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/bin/uvicorn --host="0.0.0.0" --port=8002 main:app
